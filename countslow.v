9175fcbe6031f7bb26649d373d01595966f9be40
Mridhula A <164215166+iammridhula@users.noreply.github.com>
2025-06-25 15:13:50 +0530

    Create countslow.v

---
+++ countslow.v
@@ -0,0 +1,22 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    always@(posedge clk)
+        begin
+            if(reset)
+                q<=0;
+            else
+                begin
+                    if(slowena)
+                        begin
+                        q<=q+1;
+                            if(q==4'b1001)
+                                q<=0;
+                        end
+                    else
+                        q<=q;
+                end
+        end
+endmodule
