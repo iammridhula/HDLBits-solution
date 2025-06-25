21288d176c48ee5a7415b62989d531c2cde35c07
Mridhula A <164215166+iammridhula@users.noreply.github.com>
2025-06-25 15:05:08 +0530

    Create count1to10.v

---
+++ count1to10.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input reset,
+    output [3:0] q);
+    always@(posedge clk)
+        begin
+            if(reset)
+                q<=1;
+            else
+                begin
+                q<=q+1;
+                    if(q==4'b1010)
+                        q<=1;
+                end
+        end
+endmodule
