4976b8c919e63507cffbd64a0c5c54b3e744e967
Mridhula A <164215166+iammridhula@users.noreply.github.com>
2025-07-14 14:38:21 +0530

    Create fsm2.v

---
+++ fsm2.v
@@ -0,0 +1,25 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)
+            ON:next_state=k?OFF:ON;
+            OFF:next_state=j?ON:OFF;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if(areset)
+            state<=OFF;
+        else
+            state<=next_state;
+    end
+    assign out=state;
+endmodule
